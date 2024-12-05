f47fedb2e8757e51279b999071b1de025ded841e
Madelaine Colicheo <mrcolicheom@gmail.com>
2024-12-05 01:01:15 -0400

    feat: Trabajando con urls y views

--- src/config/urls.py
+++ src/config/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("saludar/", views.saludar, name="saludar"),
+    path("saludar-con-etiqueta", views.saludar_con_etiqueta, name="saludarEtiqueta"),
+    path("saludar-con-nombre/<str:nombre>/", views.saludar_con_nombre, name="saludarNombre"),
+    path("", views.index, name="index"),
+
+
 ]
