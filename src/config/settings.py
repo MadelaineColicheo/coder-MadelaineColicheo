37069dc1a4cabb7fadb7592fd2a4148eac49ed98
Madelaine Colicheo <mrcolicheom@gmail.com>
2024-12-12 21:46:58 -0400

    Se registran las aplicaciones

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'categoria',
+    'cliente',
+    'ingredientes',
+    'pedido',
+    'producto',
 ]
 
 MIDDLEWARE = [
