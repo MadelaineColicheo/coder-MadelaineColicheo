d0b4aa3d7d8a237d008a4f4b305d7bcdd4b02800
Madelaine Colicheo <mrcolicheom@gmail.com>
2024-12-13 14:37:56 -0400

    Modificacion de base de datos

--- src/productos/models.py
+++ src/productos/models.py
@@ -11,7 +11,7 @@ class Categoria(models.Model):
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="productos")
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, related_name="productos")
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
     disponibilidad = models.BooleanField(default=True)
