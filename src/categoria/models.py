8c66d97e91f08ecd7914016c182b2c57f8d1f541
Madelaine Colicheo <mrcolicheom@gmail.com>
2024-12-12 21:58:50 -0400

    Se crean modelos

--- src/categoria/models.py
+++ src/categoria/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name="productos")
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
+    disponibilidad = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
